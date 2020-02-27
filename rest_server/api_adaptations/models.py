7274366a196bb043864f3a43769c9b447cb1bc6e
Marcel <marceli.lagoda@gmail.com>
2020-02-27 03:58:30 +0100

    Create db model movie

--- rest_server/api_adaptations/models.py
+++ rest_server/api_adaptations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from api_books.models import Book
 
-# Create your models here.
+ACTION = 1
+COMEDY = 2
+CRIME = 3
+INDEPENDENT = 4
+DOCUMENTARY = 5
+DRAMA = 6
+WAR = 7
+
+GENRES = (
+    (ACTION, 'Action'),
+    (COMEDY, 'Comedy'),
+    (CRIME, 'Crime'),
+    (INDEPENDENT, 'Independent'),
+    (DOCUMENTARY, 'Documentary'),
+    (DRAMA, 'Drama'),
+    (WAR, 'War'),
+)
+
+
+class Movie(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    director = models.CharField(max_length=256)
+    date = models.DateField()
+    genre = models.CharField(max_length=21, choices=GENRES)
