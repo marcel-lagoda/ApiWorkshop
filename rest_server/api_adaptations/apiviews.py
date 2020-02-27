2975c5f5a45178c0532a2514a57554b43829d004
Marcel <marceli.lagoda@gmail.com>
2020-02-27 15:51:12 +0100

    Add movie list API view

--- rest_server/api_adaptations/apiviews.py
+++ rest_server/api_adaptations/apiviews.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MovieList(APIView):
+
+    def get(self, request, format=None):
+        books = Movie.objects.all()
+        serializer = MovieSerializer(books, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
