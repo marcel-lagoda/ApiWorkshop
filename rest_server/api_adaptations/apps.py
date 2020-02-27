4fe4c5f515d2d1d92b954e1dc353aa6b785fadde
Marcel <marceli.lagoda@gmail.com>
2020-02-27 02:23:25 +0100

    Start new api app

---
+++ rest_server/api_adaptations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiAdaptationsConfig(AppConfig):
+    name = 'api_adaptations'
---
+++ rest_server/api_adaptations/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import apiviews
+
+urlsettings = [
+    # path('', views.as_view())
+]
