ce11e8843b75783ae0fd1909f9cad69d1532a74f
Marcel <marceli.lagoda@gmail.com>
2020-02-27 15:57:24 +0100

    Remove redundant names from api_books/urls

--- rest_server/api_books/urls.py
+++ rest_server/api_books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/book/', views.BookList.as_view()),
-    path('api/book/<int:id>', views.BookView.as_view()),
+    path('book/', views.BookList.as_view()),
+    path('book/<int:id>', views.BookView.as_view()),
 ]
