2306faeff3cadbca4ab64df643fe3b5ad8317a00
Marcel <marceli.lagoda@gmail.com>
2020-03-01 00:43:24 +0100

    Add BookSerializer to MovieSerializer
    (to access Book links)

--- rest_server/api_adaptations/serializers.py
+++ rest_server/api_adaptations/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-
+from api_books.serializers import BookSerializer
 from .models import Movie
 
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+
+    book = BookSerializer()
+
     class Meta:
         model = Movie
-        fields = ('id', 'book', 'title', 'director', 'date', 'genre')
+        fields = ('book', 'title', 'director', 'date', 'genre')
