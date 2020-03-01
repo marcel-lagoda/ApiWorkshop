a31dc1170d550e3157c9a46376b61726764069eb
Marcel <marceli.lagoda@gmail.com>
2020-03-01 02:44:10 +0100

    Change project language

--- rest_server/rest_server/settings.py
+++ rest_server/rest_server/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Warsaw'
 
