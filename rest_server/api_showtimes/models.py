2b1e2f5be35c3513ac907e01ed5a11d3a85c752d
Marcel <marceli.lagoda@gmail.com>
2020-03-01 03:34:58 +0100

    Create api_showtimes db models

--- rest_server/api_showtimes/models.py
+++ rest_server/api_showtimes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from api_adaptations.models import Movie
+
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=120)
+    city = models.CharField(max_length=120)
+    movies = models.ManyToManyField(Movie, through='Screening')
+
+
+class Screening(models.Model):
+    date = models.DateField()
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
