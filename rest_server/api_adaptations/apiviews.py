161872cbbe835291924033d22d8a85304ddeb743
Marcel <marceli.lagoda@gmail.com>
2020-03-01 02:42:26 +0100

    Add http methods for single api object

--- rest_server/api_adaptations/apiviews.py
+++ rest_server/api_adaptations/apiviews.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 
 from .models import Movie
@@ -10,8 +11,8 @@ from .serializers import MovieSerializer
 class MovieList(APIView):
 
     def get(self, request, format=None):
-        books = Movie.objects.all()
-        serializer = MovieSerializer(books, many=True, context={"request": request})
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True, context={"request": request})
         return Response(serializer.data)
 
     def post(self, request, format=None):
@@ -19,4 +20,31 @@ class MovieList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        movie = self.get_object(id)
+        serializer = MovieSerializer(movie, context={'request': request})
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        movie = self.get_object(id)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id):
+        movie = self.get_object(id)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
