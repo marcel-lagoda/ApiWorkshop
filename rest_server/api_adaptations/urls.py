59586d0de54babe341677025cd017c33474ac830
Marcel <marceli.lagoda@gmail.com>
2020-03-01 02:43:15 +0100

    Add  api urls

--- rest_server/api_adaptations/urls.py
+++ rest_server/api_adaptations/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import apiviews
 
 urlpatterns = [
-    path('movie/', apiviews.MovieList.as_view()),
+    path('movies/', apiviews.MovieList.as_view()),
+    path('movie/<int:id>', apiviews.MovieView.as_view()),
 ]
--- rest_server/api_books/urls.py
+++ rest_server/api_books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/', views.BookList.as_view()),
+    path('books/', views.BookList.as_view()),
     path('book/<int:id>', views.BookView.as_view()),
 ]
