74b5c95e814c7e6d42f870f5ff14dbd07ec4589d
Marcel <marceli.lagoda@gmail.com>
2020-03-02 02:42:14 +0100

    Register models in admin panel

--- rest_server/api_showtimes/admin.py
+++ rest_server/api_showtimes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from api_adaptations.models import Movie
+
+
+class CinemaInline(admin.TabularInline):
+    model = models.Cinema.movies.through
+
+
+class CinemaAdmin(admin.ModelAdmin):
+    model = Movie
+    inlines = [CinemaInline]
+
+
+admin.site.register(models.Cinema, CinemaAdmin)
+# admin.site.register(models.Movie)
+admin.site.register(models.Screening)
